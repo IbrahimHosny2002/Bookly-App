a72d764544a4a84b7934990bb5d4dc2b096d77ff
IbrahimHosny2002 <hosnyhema66@gmail.com>
2024-05-03 18:00:25 +0300

    add newest books cubit

---
+++ lib/features/home/presentation/view_models/newest_books_cubt/newest_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBox();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
